36d150adf7b9277c01a0cb64d5f978de6711f9ac
Code <65465165+CodeKid0@users.noreply.github.com>
2023-07-15 12:09:05 +0200

    Initial commit

---
+++ src/main/java/io/github/kurrycat2004/mpkbukkitapi/MPKApiPlugin.java
@@ -0,0 +1,13 @@
+package io.github.kurrycat2004.mpkbukkitapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MPKApiPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
